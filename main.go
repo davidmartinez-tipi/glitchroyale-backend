f660daf7fe9e3dba29ba45e833fe18554b4d6e82
David Martinez <david.martinez@tipi.com.co>
2026-03-24 10:43:31 -0500

    add cors

--- main.go
+++ main.go
@@ -11,6 +11,20 @@ import (
 	_ "github.com/lib/pq"
 )
 
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
 func main() {
 	// 1. Conexión a la BD
 	connStr := "postgresql://postgres.chpzuingsmjdocwhlqdr:E6HnkxAR8JrdkWsR@aws-0-us-west-2.pooler.supabase.com:5432/postgres"
